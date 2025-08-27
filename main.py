e5f4fbef4fdd267ff2744382d11ded26ff8082e8
seb1080 <sblaisfernandez@gmail.com>
2025-08-27 14:26:15 -0400

    feat: update main.py

--- main.py
+++ main.py
@@ -5,16 +5,6 @@ import pandas as pd
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
-
 dataSource = {
     "url": "https://geoegl.msp.gouv.qc.ca/apis/wss/historiquesc.fcgi?service=wfs&version=1.1.0&request=getfeature&typename=msp_risc_evenements_public&outputformat=CSV",
     "format": "csv",
@@ -23,7 +13,6 @@ dataSource = {
     "identifier": "8a707be3-2452-43b2-be72-fb5926876c72",
 }
 
-# %%
 try:
     response = requests.get(dataSource["url"])
     response.raise_for_status()
